0fec6d3476f3ddb05f160d2b64a2e6f01228997f
MeireContino <contino.meire@gmail.com>
2025-09-03 21:25:37 -0300

    Add laço Try catch a todos os eventos, validação da iserção do produto e correção de produtos duplicados na tela inicial

--- MauiAppComprasMensais/Models/Produto.cs
+++ MauiAppComprasMensais/Models/Produto.cs
@@ -4,11 +4,52 @@ namespace MauiAppComprasMensais.Models
 {
     public class Produto
     {
+         string _descricao;
+         double _quantidade;
+         double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco {  get; set; }
-        public double Total { get => Quantidade * Preco; }
+
+        public string Descricao 
+        { 
+            get => _descricao;
+            set 
+            {
+                if (value == null) 
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade 
+        {
+            get => _quantidade; 
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Por favor, preencha a quantidade maior que zero");
+                }
+
+                _quantidade = value;
+            }
+        }
+        public double Preco 
+        {
+            get => _preco; 
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Por favor, preencha um preço válido");
+                }
+
+                _preco = value;
+            }
+        }
+        public double Total => Quantidade * Preco;
     }
 }
